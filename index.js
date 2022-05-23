9f0521aa3e4ba43bce5fb64ba6d3c3e1eff14bb2
Chizalam <cemuchay@gmail.com>
2022-05-23 15:12:45 +0100

    .

--- index.js
+++ index.js
@@ -5,56 +5,56 @@ let operand = ''
 // add event listener to each button
 document.querySelectorAll("button").forEach(button => {
     const primaryDisplay = document.querySelector("#primary-display-value");
+    const secondaryDisplay = document.querySelector("#secondary-display-value");
 
     button.addEventListener("click", () => {
 
         displayValues.push(button.textContent)
         primaryDisplay.textContent = displayValues.join('')
+        currentValueFloat = parseFloat(currrentValue.join(''))
+        buttonValue = button.textContent
 
         if (button.textContent == '=') {
 
             if (operand == '+') {
-                result += parseFloat(currrentValue.join(''))
+                result += currentValueFloat
             }
 
             if (operand == '-') {
-                result -= parseFloat(currrentValue.join(''))
+                result -= currentValueFloat
             }
 
             if (operand == '*') {
-                result *= parseFloat(currrentValue.join(''))
+                result *= currentValueFloat
             }
 
             if (operand == '/') {
-                result /= parseFloat(currrentValue.join(''))
+                result /= currentValueFloat
             }
 
             currrentValue = [0]
-            console.log(result)
+            secondaryDisplay.textContent = result
             return
         }
 
-        if (button.textContent == "+" || button.textContent == "-" || button.textContent == "*" || button.textContent == "/") {
+        if (buttonValue == "+" || buttonValue == "-" || buttonValue == "*" || buttonValue == "/") {
 
-            result += parseFloat(currrentValue.join(''))
+            result += currentValueFloat
             currrentValue = [0]
-            operand = button.textContent
+            operand = buttonValue
             return
         }
 
         else {
-            if ((parseFloat(button.textContent) == 1 || parseFloat(button.textContent) == 2 || parseFloat(button.textContent) == 5) && operand == '') {
-                currrentValue.push(button.textContent)
+            if ((parseFloat(buttonValue) == 1 || parseFloat(buttonValue) == 2 || parseFloat(buttonValue) == 3 || parseFloat(buttonValue) == 4 || parseFloat(buttonValue) == 5 || parseFloat(buttonValue) == 6 || parseFloat(buttonValue) == 7 || parseFloat(buttonValue) == 8 || parseFloat(buttonValue) == 9 || parseFloat(buttonValue) == 0) && operand == '') {
+                currrentValue.push(buttonValue)
             }
 
             else {
-                currrentValue.push(button.textContent)
+                currrentValue.push(buttonValue)
             }
 
         }
-
-
-
 
     });
 });
